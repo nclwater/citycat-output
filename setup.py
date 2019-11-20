9874644ac53bd9c73ca99616c0241b8df666e038
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2019-11-20 15:20:20 +0000

    updated licence & url, removed upx_exclude

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='citycat_output',
-      version='0.1.0',
-      description='Convert CityCAT hydrodynamic model outputs',
-      url='https://github.com/fmcclean/citycat_output',
+      version='0.1.1',
+      description='Convert CityCAT outputs to netCDF',
+      url='https://github.com/nclwater/citycat-output',
       author='Fergus McClean',
       author_email='fergus.mcclean@ncl.ac.uk',
-      license='MIT',
+      license='GPL-3.0',
       packages=['citycat_output'],
       zip_safe=False,
       install_requires=['netCDF4',
