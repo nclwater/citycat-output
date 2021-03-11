d7fb35ca760adcd3ec66ed98ee40b0907f74446c
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2021-03-11 16:16:27 +0000

    Update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='citycatio',
-      version='0.4.1',
+      version='0.5.0',
       description='CityCAT extension to create inputs and convert outputs',
       url='https://github.com/nclwater/citycatio',
       author='Fergus McClean',
